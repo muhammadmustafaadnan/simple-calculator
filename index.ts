c86eaac7ca60bc7199911b36d4c679d017dc6614
muhammadmustafaadnan <mustafaadnan1239@gmail.com>
2024-03-28 05:40:32 +0500

    push my calculator to github

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter your first number", type: "number", name: "firstNumber" },
-  { message: "Enter your second number", type: "number", name: "secondNumber" },
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
   {
     message: "select one of the operators to perform operation",
     type: "list",
